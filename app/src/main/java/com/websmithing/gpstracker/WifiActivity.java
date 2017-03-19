93df6ac7d379d766d0936f792beb7b99fb2c6f46
Yapeng <yapeng.wang@gmail.com>
2017-03-19 13:38:22 +0800

    17 mar

--- app/src/main/java/com/websmithing/gpstracker/WifiActivity.java
+++ app/src/main/java/com/websmithing/gpstracker/WifiActivity.java
@@ -8,14 +8,18 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,15 +29,21 @@ public class WifiActivity extends Activity implements View.OnClickListener
 {
     private static final String TAG = "WifiActivity";
     WifiManager wifi;
-    ListView lv;
+    ListView lv, selectedlv;
     TextView textStatus;
-    Button buttonScan;
+    Button buttonScan, buttonAddWiFi;
     int size = 0;
     List<ScanResult> results;
+    int selectedPosition=-1;
+    int selectedToRemove=-1;
 
 
     ArrayList<HashMap<String, String>> arraylist = new ArrayList<HashMap<String, String>>();
-    SimpleAdapter adapter;
+    ArrayList<HashMap<String, String>> selectedArraylist = new ArrayList<HashMap<String, String>>();
+    SimpleAdapter adapter, selectedAdapter;
+
+
+
 
     /* Called when the activity is first created. */
     @Override
@@ -45,7 +55,10 @@ public class WifiActivity extends Activity implements View.OnClickListener
         textStatus = (TextView) findViewById(R.id.wifiTextView);
         buttonScan = (Button) findViewById(R.id.wifi_scan_button);
         buttonScan.setOnClickListener(this);
+        buttonAddWiFi=(Button) findViewById(R.id.select_ap_button);
+        buttonAddWiFi.setOnClickListener(this);
         lv = (ListView)findViewById(R.id.wifi_listView);
+        selectedlv=(ListView)findViewById(R.id.selected_WiFi);
 
         wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         if (wifi.isWifiEnabled() == false)
@@ -59,6 +72,25 @@ public class WifiActivity extends Activity implements View.OnClickListener
 
         this.adapter = new SimpleAdapter(this, arraylist, R.layout.grid_item, from, to) ;  //R.id.list_value });
         lv.setAdapter(this.adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1,int position, long arg3)
+            {
+                selectedPosition=position;
+               // Toast.makeText(WifiActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        this.selectedAdapter=new SimpleAdapter(this, arraylist, R.layout.grid_item, from, to) ;
+        selectedlv.setAdapter(this.adapter);
+        selectedlv.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1,int position, long arg3)
+            {
+                selectedToRemove=position;
+            }
+        });
 
         registerReceiver(new BroadcastReceiver()
         {
@@ -71,33 +103,39 @@ public class WifiActivity extends Activity implements View.OnClickListener
         }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
     }
 
-    public void onClick(View view)
+    public void onClick(View v)
     {
-        arraylist.clear();
-        wifi.startScan();
+        if (v.getId() == R.id.wifi_scan_button) {
 
-        Toast.makeText(this, "Scanning...." + size, Toast.LENGTH_SHORT).show();
-        try
-        {
-            size = size - 1;
-            while (size >= 0)
-            {
+            //while (size<=0) {
+            arraylist.clear();
+            wifi.startScan();
+            Log.e(TAG, "Scanned Wifi:" + size);
 
-                checkIfExist(results.get(size).SSID);
+            Toast.makeText(this, "Scanning...." + size, Toast.LENGTH_SHORT).show();
+            try {
+                size = size - 1;
+                while (size >= 0) {
 
-                if (!checkIfExist(results.get(size).SSID)) {
-                    HashMap<String, String> hashMap = new HashMap<>();
-                    hashMap.put("SSID", results.get(size).SSID);
-                    hashMap.put("BSSID", results.get(size).BSSID);
-                    arraylist.add(hashMap);
-                    adapter.notifyDataSetChanged();
+                    checkIfExist(results.get(size).SSID);
+
+                    if (!checkIfExist(results.get(size).SSID)) {
+                        HashMap<String, String> hashMap = new HashMap<>();
+                        hashMap.put("SSID", results.get(size).SSID);
+                        hashMap.put("BSSID", results.get(size).BSSID);
+                        arraylist.add(hashMap);
+                        adapter.notifyDataSetChanged();
+                    }
+                    size--;
+
                 }
-                size--;
+            } catch (Exception e) {
 
             }
+        }else if (v.getId()==R.id.select_ap_button){
+
         }
-        catch (Exception e)
-        { }
+
     }
 
     public boolean checkIfExist(String ssid){
@@ -109,4 +147,12 @@ public class WifiActivity extends Activity implements View.OnClickListener
         return false;
 
     }
+
+    private boolean saveUserSettings() {
+
+        return false;
+
+    }
+
+
 }
